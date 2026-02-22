7a53b93332d80392bf7ec94c44eec6e1b28d73c8
Aidan <96690746+CoolAid48@users.noreply.github.com>
2026-02-22 17:26:25 -0500

    Refactor KeyBindings for 1.21.9+
    1.21.9 changes the translation key format for keybinds categories

--- src/client/java/wueffi/taskmanager/client/util/KeyBindHandler.java
+++ src/client/java/wueffi/taskmanager/client/util/KeyBindHandler.java
@@ -1,23 +1,28 @@
 package wueffi.taskmanager.client.util;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import net.minecraft.client.KeyMapping;
+import net.minecraft.resources.ResourceLocation;
 import org.lwjgl.glfw.GLFW;
 import wueffi.taskmanager.client.TaskManagerScreen;
 
 public class KeyBindHandler {
 
+    private static final KeyMapping.Category CATEGORY =
+            KeyMapping.Category.register(ResourceLocation.parse("taskmanager"));
+
     private static KeyBinding openKey;
 
     public static void register() {
-        openKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+        openKey = KeyBindingHelper.registerKeyBinding(new KeyMapping(
                 "key.taskmanager.open",
-                InputUtil.Type.KEYSYM.createFromCode(GLFW.GLFW_KEY_F12),
-                "category.taskmanager"
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_F12,
+                CATEGORY
         ));
-
+        
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openKey.wasPressed()) {
                 client.setScreen(new TaskManagerScreen());
